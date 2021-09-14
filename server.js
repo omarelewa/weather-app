4de85e916670558981677468134a9226347ba66a
Omar Elewa <omar2@omar2.local>
2021-09-14 21:09:15 +0200

    Express server running on port 3000

--- server.js
+++ server.js
@@ -2,13 +2,15 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
 // Cors for cross origin allowance
 
@@ -17,3 +19,12 @@ app.use(express.static('website'));
 
 
 // Setup Server
+const port = 3000;
+
+app.get('/', (request, response)=>{
+    response.send('Hello Express :)');
+});
+
+app.listen(port, () => {
+    console.log(`Express server listening on port ${port}`);
+});
