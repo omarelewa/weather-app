4f1f7c6b0d3250ccbf50e0d7d8dd2a7e9c391626
Omar Elewa <omar2@omar2.local>
2021-09-16 01:23:15 +0200

    Ready

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
+/* eslint-disable no-console */
 // Setup empty JS object to act as endpoint for all routes
 let projectData = {
-  weatherentry: []
+  weatherentry: [],
 };
-// const api_key = 'e5935c759f600ef1895b289b2deb85f3';
 
 // Require Express to run server and routes
 const express = require('express');
@@ -17,11 +17,6 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// DEBUGGING STATIC MIDDLEWARE
-// npm install path?!
-// const path = require('path');
-// app.use(express.static(path.join(__dirname, './static')));
-
 // Cors for cross origin allowance
 app.use(cors());
 
@@ -31,42 +26,20 @@ app.use(express.static('website'));
 // Setup Server
 const port = 3000;
 
-// DEBUGGING EXPRESS SERVER INITIALIZATION
-
-// Defining an end-point to return all ads
-app.get('/all', function getProjectData(req, res) {
-  
+app.get('/all', (req, res) => {
   res.send(projectData.weatherentry);
-  
-  // res.send(projectData);
-  // console.log prints in the local terminal
-  // console.log('Hello there!');
-  // console.log(req.url);
-  // console.log(req.ip);
-  // console.log(req.hostname);
-  // console.log(req.method);
-  // console.log(req.protocol);
-  // console.log(req.path);
-  // console.log(req.query);
-  // console.log(req.subdomains);
-  // user/72  /product/223
-  // app.get("/user/:id")     app.get("/product/:id")
-  // req.params.id
-  // console.log(req.params);
 });
 
-app.post('/add', function addWeatherEntry(request, response) {
+app.post('/add', (request, response) => {
   const newData = request.body;
   console.log(newData);
   projectData.weatherentry.push(newData);
   console.log(projectData);
   response.send({
-    status: "successful"
+    status: 'successful',
   });
 });
 
-
 app.listen(port, () => {
-  // eslint-disable-next-line no-console
   console.log(`Express server listening on port ${port}`);
 });
--- website/app.js
+++ website/app.js
@@ -1,42 +1,24 @@
 /* eslint-disable no-undef */
-/* eslint-disable no-use-before-define */
-/* Global Variables */
-//const zip = '360630';
-const baseURL = 'http://api.openweathermap.org/data/2.5/weather?id=';
+// const zip = '94040';
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 const apiKey = '&appid=e5935c759f600ef1895b289b2deb85f3';
 // Create a new date instance dynamically with JS
 const d = new Date();
 const newDate = `${d.getMonth()}.${d.getDate()}.${d.getFullYear()}`;
-// eslint-disable-next-line no-undef
-// const date = document.getElementById('date');
-// date.innerHTML = newDate;
 
-document.getElementById('generate').addEventListener('click', performAction);
+const generate = document.getElementById('generate').addEventListener('click', performAction);
 
-async function performAction() {
-  const zip = document.getElementById('zip').value;
-  const feelings = document.getElementById('feelings').value;
-  const weatherdata = await getWeatherData(baseURL, zip, apiKey);
-  console.log(weatherdata);
-  const weatherEntry = {
-    date: newDate,
-    temperature: weatherdata.main.temp,
-    user_response: feelings,
-  };
-  const post_result = await postWeatherEntry('/add', weatherEntry);
-  console.log(post_result);
-  await printLatestEntry('/all');
-}
-
-const getWeatherData = async (baseURL, zip, apiKey) => {
+const fetchWeatherData = async (baseURL, zip, apiKey) => {
+  // construct API URL for fetch
   const url = baseURL + zip + apiKey;
   const res = await fetch(url);
+  // convert the body into json
   const weatherData = await res.json();
   return weatherData;
 };
 
-const postWeatherEntry = async (url = '/add', data = {}) => {
-  const response = await fetch(url, {
+const saveWeatherData = async (url = '/add', data = {}) => {
+  const res = await fetch(url, {
     method: 'POST',
     credentials: 'same-origin',
     headers: {
@@ -44,18 +26,47 @@ const postWeatherEntry = async (url = '/add', data = {}) => {
     },
     body: JSON.stringify(data),
   });
-  const result = await response.json();
-  return result;
+  const newWeatherEntry = await res.json();
+  return newWeatherEntry;
 };
 
+// manipulate HTML file for dynamic UI
 const printLatestEntry = async (url = '/all') => {
-  const response = await fetch(url);
-  const data = await response.json();
-  const date = (document.getElementById('date').innerHTML = `Date: ${data[data.length - 1].date}`);
-  const temp = (document.getElementById('temp').innerHTML = `Temperature: ${
-    data[data.length - 1].temperature
-  }`);
-  const content = (document.getElementById('content').innerHTML = `Feelings: ${
-    data[data.length - 1].user_response
-  }`);
+  const res = await fetch(url);
+  const data = await res.json();
+  document.getElementById('date').innerHTML = `Date: ${data[data.length - 1].date}`;
+  document.getElementById('temp').innerHTML = `Temperature: ${data[data.length - 1].temperature}`;
+  document.getElementById('content').innerHTML = `Feelings: ${data[data.length - 1].user_feelings}`;
 };
+
+
+async function performAction() {
+  // capture zip value
+  const zip = document.getElementById('zip').value;
+  // check for zip code absence
+  if (!zip) {
+    alert('Please enter a ZIP code!');
+    return;
+  }
+  // capture feelings value
+  const feelings = document.getElementById('feelings').value;
+  // check for feelings absence
+  if (!feelings) {
+    alert('Please enter a valid journal entry!');
+    return;
+  }
+  // go fetch weather data from external api
+  const weatherdata = await fetchWeatherData(baseURL, zip, apiKey);
+  // check for weather data absence
+  if (!weatherdata) {
+    alert('Error while getting temperature from the entered ZIP code');
+    return;
+  }
+  const weatherEntry = {
+    date: newDate,
+    temperature: weatherdata.main.temp,
+    user_feelings: feelings,
+  };
+  await saveWeatherData('/add', weatherEntry);
+  await printLatestEntry('/all');
+}
